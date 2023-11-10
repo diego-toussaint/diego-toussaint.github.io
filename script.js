e98b6a38c65211b90d2ab967a50b5423d4b9a113
Diego Toussaint <150408319+diego-toussaint@users.noreply.github.com>
2023-11-09 22:11:25 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const skillsList = document.getElementById('skills');
+
+function toggleSkills() {
+    skillsList.classList.toggle('hidden');
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
